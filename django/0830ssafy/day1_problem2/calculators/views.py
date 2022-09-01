9bc40e7bfbded94926b76cdac4a753bdf76bda56
WoongKi1115 <nwk6638@gmail.com>
2022-09-02 01:11:28 +0900

    0901

--- django/0830ssafy/day1_problem2/calculators/views.py
+++ django/0830ssafy/day1_problem2/calculators/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def calculation(request):
+    return render(request, 'calculators/calculation.html')
+
+def result(request):
+    first=request.GET.get('first')
+    second=request.GET.get('second')
+    context = {
+        'first':first,
+        'second':second
+    }
+    return render(request, 'calculators/result.html', context)
--- django/0830ssafy/day1_problem2/project2/urls.py
+++ django/0830ssafy/day1_problem2/project2/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('calculators/', include('calculators.url'))
+    path('calculators/', include('calculators.urls'))
 ]
